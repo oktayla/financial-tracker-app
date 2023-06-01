5ec04b36ad6f353161add2c25d1fc13c3065915e
oktayla <oktigh@gmail.com>
2023-06-01 05:02:46 +0300

    [feature] Resources and Requests have been added for Transaction

---
+++ app/Http/Requests/TransactionRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\TransactionStatus;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use App\Models\TransactionCategory;
+
+class TransactionRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'category_id' => [
+                'required',
+                'integer',
+                Rule::exists((new TransactionCategory)->getTable(), 'id')
+                    ->where('type', $this->get('type')),
+            ],
+            'type' => [
+                'required',
+                'string',
+                Rule::in([
+                    TransactionStatus::Income,
+                    TransactionStatus::Expense,
+                ])
+            ],
+            'amount' => ['required', 'decimal:10'],
+        ];
+    }
+}
---
+++ app/Http/Resources/TransactionResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TransactionResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'category' => new TransactionCategoryResource($this->category),
+            'type' => $this->type,
+            'amount' => $this->amount,
+            'created_at' => $this->created_at,
+        ];
+    }
+}
